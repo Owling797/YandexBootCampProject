27a3d22f835bae4a7e6c111aa5d181c75e4054b1
Owling797 <zavadskayalus@gmail.com>
2024-04-10 03:53:09 +0300

    added app skeleton

---
+++ app.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import streamlit as st
+from PIL import Image
+from model import open_and_preprocess_data, get_tokenizer, load_model_and_generate
+
+
+def process_main_page():
+    show_main_page()
+    process_side_bar_inputs()
+
+
+def show_main_page():
+    #image = Image.open('data/titanic.jpg')
+    st.set_page_config(
+        layout="wide",
+        initial_sidebar_state="auto",
+        page_title="Cringe Songs Generation",
+        #page_icon=image,
+
+    )
+    st.write(
+        """
+        Generate your own song
+        """
+    )
+    #st.image(image)
+
+
+def write_generation_result(prediction_result):
+    st.write("## Result")
+    st.write(prediction_result)
+
+
+def process_side_bar_inputs():
+    st.sidebar.header('Input parameters')
+    input_features = sidebar_input_features()
+    #model_type, prompt = input_features['model_type'], input_features['prompt']
+    if input_features['model_type'] == "RuGPT finetuned":
+        #train_path = open_and_preprocess_data()
+        tokenizer = get_tokenizer()
+        generation_result = load_model_and_generate(input_features['prompt'], tokenizer)
+        write_generation_result(generation_result)
+
+    else:
+        write_generation_result("Another model")
+
+def sidebar_input_features():
+    model_type = st.sidebar.selectbox("Model", ("Self-made", "RuGPT finetuned"))
+    prompt = st.text_input('Lyrics begin with:', 'Добрым словом')
+    data = {
+        "model_type": model_type,
+        "prompt": prompt,
+    }
+    return data
+
+
+if __name__ == "__main__":
+    process_main_page()
---
+++ model.py
@@ -0,0 +1,108 @@
+from pickle import dump, load
+import pandas as pd
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, TextDataset, DataCollatorForLanguageModeling, Trainer, TrainingArguments
+import torch
+import os
+from google_drive_connector import GoogleDriveConnector
+
+
+
+def split_data(df: pd.DataFrame):
+    y = df['Survived']
+    X = df[["Pclass", "Sex", "Age", "SibSp", "Parch", "Embarked"]]
+
+    return X, y
+
+def open_and_preprocess_data(folder_path="data/texts"):
+    train_path = 'data/train_dataset.txt'
+    with open(train_path, "w", encoding="utf-8") as train_file:
+        for filename in os.listdir(folder_path):
+            if filename.endswith('.txt'):
+                file_path = os.path.join(folder_path, filename)
+                with open(file_path, "r", encoding="utf-8") as file:
+                    train_file.write(file.read() + '\n')
+
+    with open(train_path, 'r') as f:
+        lines = f.readlines()
+    # Filter out empty lines
+    non_empty_lines = [line for line in lines if line.strip()]
+    n = 480
+    non_empty_lines = non_empty_lines[:n]
+
+    train_path = 'data/train_dataset_stripped.txt'
+    with open(train_path, 'w', encoding="utf-8") as f:
+        f.writelines(non_empty_lines)
+        
+    return train_path
+
+def get_tokenizer():
+    model_name_or_path = "sberbank-ai/rugpt3large_based_on_gpt2"
+    tokenizer = GPT2Tokenizer.from_pretrained(model_name_or_path)
+    '''model = GPT2LMHeadModel.from_pretrained(model_name_or_path).to(DEVICE)
+    
+    # Создание датасета
+    train_dataset = TextDataset(tokenizer=tokenizer,file_path=train_path,block_size=64)
+    
+    # Создание даталодера (нарезает текст на оптимальные по длине куски)
+    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, 
+                                                    mlm=False)
+    training_args = TrainingArguments(
+        output_dir="./finetuned", # The output directory
+        overwrite_output_dir=True, # Overwrite the content of the output dir
+        num_train_epochs=200, # number of training epochs
+        per_device_train_batch_size=32, # batch size for training
+        per_device_eval_batch_size=32,  # batch size for evaluation
+        warmup_steps=10, # number of warmup steps for learning rate scheduler
+        gradient_accumulation_steps=16, # to make "virtual" batch size larger
+        fp16=True,
+        )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        data_collator=data_collator,
+        train_dataset=train_dataset,
+        optimizers = (torch.optim.AdamW(model.parameters(),lr=1e-5), None)
+    )
+    
+    trainer.train()
+    
+    file_path = "model_rugpt3large.pkl"
+    with open(file_path, "wb") as file:
+        dump(model, file)
+
+    print(f"Model was saved to {file_path}")
+    '''
+    return tokenizer
+
+
+def load_model_and_generate(prompt, tokenizer):
+    connector = GoogleDriveConnector()
+    file_id = '1gIDQlNkrGToqN8nrjtWrmtGLDvioheLh'
+    local_path = 'data/model_rugpt3large.pkl'
+    connector.download_file(file_id, local_path)
+    with open(local_path, "rb") as file:
+        model = load(file)
+
+    DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    input_ids = tokenizer.encode(prompt, return_tensors="pt").to(DEVICE)
+
+    model.eval()
+    with torch.no_grad():
+        out = model.generate(input_ids,
+                            do_sample=True,
+                            num_beams=2,
+                            temperature=3.5,
+                            top_p=0.9,
+                            max_length=128,
+                            )
+
+    generated_text = list(map(tokenizer.decode, out))[0]
+
+    return generated_text
+
+
+if __name__ == "__main__":
+    train_path = open_and_preprocess_data()
+    tokenizer = get_tokenizer()
+    load_model_and_generate(prompt, tokenizer)
